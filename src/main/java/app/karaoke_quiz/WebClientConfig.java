5b39cb10d7db02e927874d0baf99c02a40037956
Roberto Albergo <ralbergo7@gmail.com>
2025-08-16 03:41:38 +0200

    ddd

--- src/main/java/app/karaoke_quiz/WebClientConfig.java
+++ src/main/java/app/karaoke_quiz/WebClientConfig.java
@@ -8,17 +8,25 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
-    @Bean
+    @Value("${deezer.rapidapi.key}")
+    private String deezerApiKey;
+
+    @Value("${deezer.rapidapi.host}")
+    private String deezerApiHost;
+
+    @Bean("deezerRapidApiClient")
     public WebClient deezerRapidApiClient(WebClient.Builder builder) {
         return builder
-                .baseUrl("https://api.deezer.com") // URL base Deezer
+                .baseUrl("https://deezerdevs-deezer.p.rapidapi.com")
+                .defaultHeader("X-RapidAPI-Key", deezerApiKey)
+                .defaultHeader("X-RapidAPI-Host", deezerApiHost)
                 .build();
     }
 
-    @Bean
+    @Bean("lyricsOvhClient")
     public WebClient lyricsOvhClient(WebClient.Builder builder) {
         return builder
-                .baseUrl("https://api.lyrics.ovh/v1") // URL base Lyrics.ovh
+                .baseUrl("https://api.lyrics.ovh/v1")
                 .build();
     }
 }
